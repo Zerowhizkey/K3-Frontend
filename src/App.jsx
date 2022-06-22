b3c54ffd534d31fb80d1cb9e29708a1565eb5aba
Zerowhizkey <marcus.piri@cmeducations.se>
2022-06-22 15:49:54 +0200

    fixed username and saved messages

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,31 @@ function App() {
 	const [message, setMessage] = useState("");
 	const [messages, setMessages] = useState([]);
 	const [user, setUser] = useState("");
+	const [username, setUsername] = useState("");
+	const [room, setRoom] = useState("");
+	const [showChat, setShowChat] = useState(false);
+	// && room !== ""
+	const chooseUsername = () => {
+		if (username !== "") {
+			socket.emit("choose_username", username);
+			socket.emit("join_room", room);
+			setShowChat(true);
+		}
+	};
 
 	useEffect(() => {
 		socket.on("connection", () => {
 			// console.log("works?!!=!");
 		});
-		socket.on("messages", ({ message, user }) => {
+
+		socket.on("befintligamedelanden", (data) => {
+			setMessages(data);
+		});
+		socket.on("messages", ({ message, user, id }) => {
 			console.log(message);
+			console.log(id);
 			setMessages((prevMessage) => {
-				return [...prevMessage, { message, user }];
+				return [...prevMessage, { message, user, id }];
 			});
 		});
 		socket.on("user", (data) => {
@@ -27,48 +43,72 @@ function App() {
 	}, []);
 
 	function handleMessage(message) {
-		socket.emit("messages", { message, user });
+		socket.emit("messages", message);
 	}
 
 	return (
 		<div className="App">
-			<header className="App-header">
-				<div className="application">
-					<div className="appLayout">
-						<div className="sideBar">
-							<p>ROOMS:</p>
-						</div>
-						<div className="chatLayout">
-							<div className="chat">
-								{messages.map((message) => (
-									<div>
-										<p className="userId">
-											{message.user}:
-										</p>
-										<p className="message">
-											{message.message}
-										</p>
-									</div>
-								))}
+			{!showChat ? (
+				<div className="joinChatContainer">
+					<h3>Choose a username</h3>
+					<input
+						type="text"
+						placeholder="John..."
+						onChange={(event) => {
+							setUsername(event.target.value);
+						}}
+					/>
+					{/* <input
+						type="text"
+						placeholder="Room ID..."
+						onChange={(event) => {
+							setRoom(event.target.value);
+						}}
+					/> */}
+					<button onClick={chooseUsername}>Accept</button>
+				</div>
+			) : (
+				<header className="App-header">
+					<div className="application">
+						<div className="appLayout">
+							<div className="sideBar">
+								<p>ROOMS:</p>
 							</div>
-							<div className="inputLayout">
-								<input
-									onChange={(e) => setMessage(e.target.value)}
-									className="chatInput"
-									type="text"
-									placeholder="Message"
-								></input>
-								<button
-									onClick={() => handleMessage(message)}
-									className="inputButton"
-								>
-									Send
-								</button>
+							<div className="chatLayout">
+								<div className="chat">
+									{messages.map((message) => (
+										<div key={message.id}>
+											{/* {message.user}: */}
+											<p className="userId">
+												{message.user.username}:
+											</p>
+											<p className="message">
+												{message.message}
+											</p>
+										</div>
+									))}
+								</div>
+								<div className="inputLayout">
+									<input
+										onChange={(e) =>
+											setMessage(e.target.value)
+										}
+										className="chatInput"
+										type="text"
+										placeholder="Message"
+									></input>
+									<button
+										onClick={() => handleMessage(message)}
+										className="inputButton"
+									>
+										Send
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</header>
+				</header>
+			)}
 		</div>
 	);
 }
